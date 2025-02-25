f013d28f291a66fea72a238bcace64576e7b172f
Sebastian Ruiz <107374004+SebasDevelop25@users.noreply.github.com>
2025-02-25 06:49:01 -0500

    poniendo mi segundo nombre en codigo.py

--- codigo.py
+++ codigo.py
@@ -1,5 +1,5 @@
-nombre = "Soy"
-apellido = "Dalto"
+nombre = "Sebastian David"
+apellido = "EL f"
 
 print("Hola " + nombre + " " + apellido + " " + "¿cómo estás?")
 saludo = "Hola " + nombre + " " + apellido
